026fafc2490dce1e22bdbb2124322180551160ab
Jake Alford <jake.e.alford@gmail.com>
2020-11-04 09:28:49 -0500

    renamed data_log to tlog

--- demo_serial_data/demo_serial_data.ino
+++ demo_serial_data/demo_serial_data.ino
@@ -1,16 +1,18 @@
+// Testing code used to supply serial data to the temperature plotting script
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("secs/4    BLU    GRN     RED     YELLOW  Setting   Error   Int   Der   Action     ");
- 
+
 }
 
  float   a = 0;
- 
+
 void loop() {
-  // put your main code here, to run repeatedly:  
-  
-  
+  // put your main code here, to run repeatedly:
+
+
 
   float b = random(1000, 5000) / 100.0;
   float c = random(1000, 5000) / 100.0;
@@ -19,14 +21,14 @@ void loop() {
   float f = random(1000, 5000) / 100.0;
   float g = random(1000, 5000) / 100.0;
   float h = random(1000, 5000) / 100.0;
-  float i = random(1000, 5000) / 100.0;  
+  float i = random(1000, 5000) / 100.0;
   float j = random(1000, 5000) / 100.0;
-  float k = random(1000, 5000) / 100.0;  
+  float k = random(1000, 5000) / 100.0;
 
   Serial.print(a,0);
   Serial.print("   ");
   Serial.print(b);
-  Serial.print("   ");  
+  Serial.print("   ");
   Serial.print(c);
   Serial.print("   ");
   Serial.print(d);
@@ -34,7 +36,7 @@ void loop() {
   Serial.print(e);
   Serial.print("   ");
   Serial.print(f);
-  Serial.print("   ");  
+  Serial.print("   ");
   Serial.print(g);
   Serial.print("   ");
   Serial.print(h);
