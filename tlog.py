9062adcc02c7b5b10a9da9f64d7869e5dc624cd9
Jake Alford <jake.e.alford@gmail.com>
2021-03-02 08:44:26 -0500

    updated tlog with proper timing but no graphing

--- tlog.py
+++ tlog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/e nv python3
 
 import sys
 import argparse
@@ -29,7 +29,8 @@ ser = serial.Serial(port, baud)
 
 # Setup output file
 writer = csv.writer(open(args["output"], "w"))
-header = ["Time Stamp", "deltaT", "t/4", "", "BLU", "GRN", "RED", "YELLOW", "SET", "Error"]
+header = ["Time Stamp", "deltaT", "t/4", "TC1", "TC2", "TC3", "TC4", "TC5", "TC6",
+          "TC7", "TC8", "SET", "Error", "Int", "Der", "Ontime"]
 writer.writerow(header)
 
 # Create figure for plotting data
@@ -42,19 +43,35 @@ plt.style.use("fivethirtyeight")
 # Create lists for data
 deltaT = [] #store trials here (n)
 # Thermocouple lists
-blu = []
-grn = [] 
-red = []
-yel = []
+
+tc1 = []
+tc2 = []
+tc3 = []
+tc4 = []
+tc5 = []
+tc6 = []
+tc7 = []
+tc8 = []
 
 # Index of the columns for each data type in the parsed serial read
 deltaT_col = 1
-blu_col = 4
-grn_col = 5
-red_col = 6
-yel_col = 7
-set_col = 8
-err_col = 9
+
+tc1_col = 3
+tc2_col = 4
+tc3_col = 5
+tc4_col = 6
+
+tc5_col = 10 #7
+tc6_col = 9 #8
+tc7_col = 8 #9
+tc8_col = 7 #10
+
+#blu_col = 4
+#grn_col = 5
+#red_col = 6
+#yel_col = 7
+set_col = 11
+err_col = 12
 
 # Used to compute deltaT
 t0 = datetime.now()
@@ -63,7 +80,7 @@ t0 = datetime.now()
 def parse_serial_read(line):
     parse_list = []
 
-    timestamp =  datetime.now().strftime("%c")
+    timestamp = datetime.now().strftime("%c")
     parse_list.append(timestamp)
 
     # timedelta from start of data collection
@@ -73,31 +90,61 @@ def parse_serial_read(line):
 
     # parse the serial data
     line = line.decode("utf-8", errors='ignore')
-    # currently delimited by '   ' but ideally could be \t for consistancy
-    line_as_list=line.split('   ')
+    # delimited by \t 
+    line_as_list=line.split('\t')
 
     parse_list = parse_list + line_as_list
-
+    
+    
     return parse_list
 
+# Helper function to check for valid serial read
+def validate_parse(line):
+
+    if len(line) is not 16:
+        return False
+
+    # check last 13 elements for vaild float values, discard if not valid
+    valid_line = line[-13:]
+    for i in range(len(valid_line)):
+        try:
+            valid_line[i] = float(valid_line[i])
+        # print error and failed parse to console for troubleshooting
+        except Exception as ex:
+            print ("************* Parse failed: ********************")
+        
+            for x in header[1:]:
+                print(x, end ="\t")
+            print()
+            for x in line[1:]:
+                print(x, end="\t")
+            print()
+            print()
+            
+            return False
+
+    # join list with time stamp
+    valid_line = line[:3] + valid_line
+
+
+    print ("Parse success:")
+        
+    for x in header[1:]:
+        print(x, end ="\t")
+    print()
+    for x in valid_line[1:]:
+        print(x, end="\t")
+    print()
+   
+    
+    # write line to csv file
+    writer.writerow(valid_line)
+    return (valid_line)
+  
+
 # Helper function to plot the parsed data into the figure and write to csv.
-# Does some basic serial read error checking.
 def plot_data(line):
-
-    # sometimes the serial data comes in incomplete so this disgards those
-    if len(line) is not 11:
-        return
-    # the first several serial reads can be 'corrupted' so discard the first 5
-    if int(line[1]) < 5:
-        return
-
-    # add data points from line to each list
-    deltaT.append(line[deltaT_col])
-    blu.append(float(line[blu_col]))
-    grn.append(float(line[grn_col]))
-    red.append(float(line[red_col]))
-    yel.append(float(line[err_col]))
-
+    
     set = "Set Point: " + line[set_col]
     error = "Error: " + line[err_col]
 
@@ -105,10 +152,84 @@ def plot_data(line):
     plt.cla()
     plt.title('TVAC Data')
 
-    plt.plot(deltaT, blu, "b", label="BLU", linewidth=1)
-    plt.plot(deltaT, grn, "g", label="GRN", linewidth=1)
-    plt.plot(deltaT, red, "r", label="RED", linewidth=1)
-    plt.plot(deltaT, yel, "y", label="YELLOW", linewidth=1)
+
+    # add data points from line to each list
+    '''
+    try:
+        deltaT.append(line[deltaT_col])
+        #plt.plot(deltaT, tc1, label="tc1", linewidth=1)
+    except Exception as e:
+        print(e)
+    '''
+    #print(line)
+    try:
+        #print(float(line[tc1_col]))
+        tc1.append(float(line[tc1_col]))
+        #plt.plot(deltaT, tc1, "b", label="tc1", linewidth=1)
+    except Exception as e:
+        print ("tc1 error: ")
+        print(e)
+
+    '''
+    try:
+        tc2.append(float(line[tc2_col]))
+        plt.plot(deltaT, tc2, label="tc2", linewidth=1)
+    except Exception as e:
+        print ("tc2 error: ")
+        print(e)
+    try:
+        tc3.append(float(line[tc3_col]))
+        plt.plot(deltaT, tc3, label="tc3", linewidth=1)
+    except Exception as e:
+        print ("tc3 error: ")
+        print(e)
+    try:
+        tc4.append(float(line[tc4_col]))
+        plt.plot(deltaT, tc4, label="tc4", linewidth=1)
+    except Exception as e:
+        print ("tc4 error: ")
+        print(e)
+    try:
+        tc5.append(float(line[tc5_col]))
+        plt.plot(deltaT, tc5, label="tc5", linewidth=1)
+    except Exception as e:
+        print ("tc5 error: ")
+        print(e)
+    try:
+        tc6.append(float(line[tc6_col]))
+        plt.plot(deltaT, tc6, label="tc6", linewidth=1)
+    except Exception as e:
+        print ("tc6 error: ")
+        print(e)
+    try:
+        tc7.append(float(line[tc7_col]))
+        plt.plot(deltaT, tc7, label="tc7", linewidth=1)
+    except Exception as e:
+        print ("tc7 error: ")
+        print(e)
+    try:
+        tc8.append(float(line[tc8_col]))
+        plt.plot(deltaT, tc8, label="tc8", linewidth=1)
+    except Exception as e:
+        print ("tc8 error: ")
+        print(e)
+    '''
+
+        
+        
+
+
+    
+    '''
+    plt.plot(deltaT, tc1, "b", label="tc1", linewidth=1)
+    plt.plot(deltaT, tc2, "g", label="tc2", linewidth=1)
+    plt.plot(deltaT, tc3, "r", label="tc3", linewidth=1)
+    plt.plot(deltaT, tc4, "y", label="tc4", linewidth=1)
+    plt.plot(deltaT, tc5, "b", label="tc5", linewidth=1)
+    plt.plot(deltaT, tc6, "g", label="tc6", linewidth=1)
+    plt.plot(deltaT, tc7, "r", label="tc7", linewidth=1)
+    plt.plot(deltaT, tc8, "y", label="tc8", linewidth=1)
+   
 
     plt.legend()
     plt.xlabel("Time (seconds)")
@@ -116,27 +237,33 @@ def plot_data(line):
 
     plt.text(0.02, 0.45, set, fontsize=14, transform=plt.gcf().transFigure)
     plt.text(0.02, 0.40, error, fontsize=14, transform=plt.gcf().transFigure)
-
+    ''' 
+    
     # write data to .csv file
-    writer.writerow(line)
+    #writer.writerow(line)
 
 if __name__ == '__main__':
     try:
         print ("Flushing Serial Data...")
-        def animate(i):
+        while 1:
+        #def animate(i):
             #if i < 10:
             #    return
             #get serial data and parse and plot
             line = ser.readline().strip()
             line = parse_serial_read(line)
-            plot_data(line)
-            print (line)
+            line = validate_parse(line)
+            if line:
+                print()
+                
+            #plot_data(line)
+            #print (line)
+            #print(tc1) 
 
 
-
-        ani = FuncAnimation(plt.gcf(), animate, interval=1000)
-        plt.tight_layout()
-        plt.show()
+        #ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+        #plt.tight_layout()
+        #plt.show()
 
 
     except KeyboardInterrupt:
